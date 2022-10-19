0fc07d06f4133f80440c492f5b9ea3beab50a3c7
Ana Beatriz <anabsenaf@gmail.com>
2022-10-18 22:13:56 -0300

    Commit

--- JS/script.js
+++
@@ -1,6 +0,0 @@
-const menuMobile = document.getElementById("menuMobile");
-function toggleMenu(){
-    const menu = document.getElementById("menu");
-    menu.classList.toggle("active")
-}
-menuMobile.addEventListener("click", toggleMenu)
